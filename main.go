0038310be497b741e358ba81b380fe5756f146f5
danauerbach <dauerbach@ucsd.edu>
2021-01-20 16:42:13 -0800

    trap for errors looking for user nrts, but allow app to run even if no such user

--- main.go
+++ main.go
@@ -20,19 +20,19 @@ import (
 	"log"
 	"log/syslog"
 	"os"
-    "os/user"
-    "path/filepath"
-    "strconv"
-    "syscall"
+	"os/user"
+	"path/filepath"
 	"rpm/cmd"
 	rlog "rpm/log"
+	"strconv"
+	"syscall"
 )
 
 type ctxKeyType string
 
 func main() {
 
-    var err error
+	var err error
 
 	err = rlog.InitLogging("rpm", syslog.LOG_LOCAL0|syslog.LOG_NOTICE)
 	if err != nil {
@@ -47,22 +47,27 @@ func main() {
 	}
 	rlog.NoticeMsg(fmt.Sprintf("%s starting up...", abspath))
 
+	var uid, gid int
+	nrtsuser, err := user.Lookup("nrts")
+	if err != nil {
+		rlog.WarningMsg("user nrts not found")
+	} else {
+		if uid, err = strconv.Atoi(nrtsuser.Uid); err != nil {
+			rlog.ErrMsg("could not convert uid %v to int", uid)
+			log.Fatal("error converting UID to int")
+		}
+		if gid, err = strconv.Atoi(nrtsuser.Gid); err != nil {
+			rlog.ErrMsg("could not convert gid %v to int", gid)
+			log.Fatal("error converting GID to int")
+		}
+		syscall.Setuid(uid)
+		syscall.Setgid(gid)
+		os.Chdir(nrtsuser.HomeDir)
+	}
 
-    var uid, gid int
-    nrtsuser, err := user.Lookup("nrts")
-    if uid, err = strconv.Atoi(nrtsuser.Uid); err != nil {
-        log.Fatal("ERROR converting UID to Int")
-    }
-    if gid, err = strconv.Atoi(nrtsuser.Gid); err != nil {
-        log.Fatal("ERROR converting GID to Int")
-    }
-    syscall.Setuid(uid)
-    syscall.Setgid(gid)
-    os.Chdir(nrtsuser.HomeDir)
-
-    var wd string
-    wd, err = os.Getwd()
-    rlog.NoticeMsg(fmt.Sprintf("working in: %s", wd))
+	var wd string
+	wd, err = os.Getwd()
+	rlog.NoticeMsg(fmt.Sprintf("working in: %s", wd))
 
 	cmd.Execute()
 }
